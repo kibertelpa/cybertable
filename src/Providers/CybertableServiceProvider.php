fddeacebb70099c06a9de4da061df6b97b2d2132
Māris Reinolds <marisreinolds@gmail.com>
2020-09-23 21:39:24 +0300

    First commit

---
+++ src/Providers/CybertableServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kibertelpa\Cybertable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CybertableServiceProvider extends ServiceProvider {
+	
+	public function boot() {
+		
+	}
+	
+	public function register() {
+		
+	}
+	
+}
